ab9af03facd4744b7411d5d1b3d995cee2ff9d82
aa235553756 <aa235553756@gmail.com>
2022-09-14 18:06:13 +0800

    Update 2022-09-14T10:06:07.523Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,25 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  } // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+
 });
 //# sourceMappingURL=all.js.map
